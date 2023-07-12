025b3eee0b77e7f7cf70fca1b755a46165e24f8f
Dustin <dustin99wii@gmail.com>
2023-07-12 00:17:56 -0400

    SP, Halt, Exit Pass

--- pintosUP_VM_FS/src/userprog/syscall.c
+++ pintosUP_VM_FS/src/userprog/syscall.c
@@ -27,138 +27,49 @@ syscall_init (void)
 }
 
 static bool
-is_valid_addr(void* addr){
-    uint32_t* page_ptr = page_lookup (thread_current ()->pagedir, addr);
+is_valid_range(void* addr_begin, void* addr_end) {
+//    printf("Is valid range\n");
+    bool begin = is_valid_addr (addr_begin);
+//    printf("begin addr: 0x%x, begin bool: %d\n", addr_begin, begin != 0);
+    bool end = is_valid_addr(addr_end);
+//    printf("end: 0x%x, end bool: %d\n", addr_end, end != 0);
+    return begin & end;
+}
+
+static bool
+is_valid_addr(void* addr) {
+//    printf("is_valid_addr, addr: 0x%x\n", addr);
+    if(!addr){
+//        printf("NULL!: 0x%x\n", addr);
+        return false;
+    }
+
+    if(!is_user_vaddr (addr)) {
+        return false;
+    }
+
+//    uint32_t* page_ptr = page_lookup (thread_current ()->pagedir, addr);
+    uint32_t* page_ptr = pagedir_get_page(thread_current ()->pagedir, addr);
 
 //    if(!page_ptr){
-//        printf("NOT VALID!: 0x%x\n", addr);
+////        printf("Not valid address!: 0x%x\n", addr);
 //    }
     return page_ptr != NULL;
 }
-//
-//static void
-//syscall_handler2 (struct intr_frame *f UNUSED)
-//{
-//    printf("System Call!\n");
-//    struct thread *t = thread_current ();
-//    if(t){
-//        printf("%s: exit(%d)\n", t->name, t->exit_status);
-//    } else {
-//        printf("T is bad!\n");
-//    }
-//
-//    thread_exit ();
-//    //thread_exit();
-//    return;
-//    int *esp = (int*)f->esp;
-////    if (!is_valid_addr(p))
-////        exit_handler(-1);
-////  hex_dump(f->esp, f->esp, 50, true);
-//    int number = *esp;
-//    //printf("number: %d\n", number);
-//    esp += 1;
-//
-//    bool b_handled = false;
-//
-//    switch(number){
-//        case SYS_HALT:                   /* Halt the operating system. */
-//            halt_handler();
-//            break;
-//        case SYS_EXIT:
-//        {
-//
-////            printf("SYS_EXIT\n");
-////            //hex_dump(f->esp, f->esp, 80, true);
-////            if(!is_user_vaddr (esp)){
-////                //b_handled = true;
-////                // printf("Invalid virtual address: 0x%x\n", esp);
-//////              exit_handler(-1);
-////            }
-////            {
-////                //this is what makes it not spin
-////                int* status_ptr = esp;
-////                exit_handler(*status_ptr);
-////            }
-////            b_handled = true;
-//        }
-//            break;
-////        case SYS_EXEC:                   /* Start another process. */
-////            break;
-////        case SYS_WAIT:                   /* Wait for a child process to die. */
-////            break;
-////        case SYS_CREATE:                 /* Create a file. */
-////            break;
-////        case SYS_REMOVE:                 /* Delete a file. */
-////            break;
-////        case SYS_OPEN:                   /* Open a file. */
-////            break;
-////        case SYS_FILESIZE:               /* Obtain a file's size. */
-////            break;
-////        case SYS_READ:                   /* Read from a file. */
-////            break;
-//        case SYS_WRITE: /* Write to a file. */
-//        {
-//            printf("SYS_WRITE\n");
-//            //hex_dump(f->esp, f->esp, 50, true);
-//            int fd = *esp;
-//            esp += 1;
-////            if (!is_valid_addr(esp))
-////                exit_handler(-1);
-//            char *buffer = *esp;
-//            esp += 1;
-//            unsigned size = *esp;
-//            esp += 1;
-//            //printf("fd: %d, buffer: %s, buffer address: 0x%x,  size: %d\n", fd, buffer, buffer, size);
-//            f->eax = write_handler(fd, buffer, size);
-//            b_handled = true;
-//        }
-//            break;
-////        case SYS_SEEK:                   /* Change position in a file. */
-////            break;
-////        case SYS_TELL:                   /* Report current position in a file. */
-////            break;
-////        case SYS_CLOSE:                  /* Close a file. */
-////            break;
-//        default:
-//            printf ("unknown syscall: %d\n", number);
-//            b_handled = true;
-//            break;
-//    }
-//
-//    if(!b_handled)
-//        thread_exit();
-//
-////------------------------------------------------------------------
-//
-//
-//
-////  printf ("system call!\n");
-////    int* esp = (int*)f->esp;
-//    //hex_dump(f->esp, f->esp, 50 , true);
-////    printf("0x%x\n", *(esp ));
-////    printf("0x%x\n", *(esp + 1));
-////
-////  if(!is_user_vaddr (*(esp+1))){
-////      printf("Invalid virtual addrss\n");
-////  }
-////  if(!is_valid_addr(*(esp+1)))
-////    exit_handler(-1);
-////    thread_exit();
-//
-////to get the sc-bad-sp tp print
-//    //thread_exit();
-////    exit_handler(-1);
-//}
+
 
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
+
   int *esp = (int*)f->esp;
-    if (!is_valid_addr(esp))
+    if(!is_valid_range(f->esp, f->esp + 3))
         exit_handler(-1);
-//  hex_dump(f->esp, f->esp, 50, true);
+//    if (!is_valid_addr(esp))
+//        exit_handler(-1);
+  //hex_dump(f->esp, f->esp, 80, true);
   int number = *esp;
-  //printf("number: %d\n", number);
+//  printf("number: %d\n", number);
   esp += 1;
 
   bool b_handled = false;
@@ -169,16 +80,22 @@ syscall_handler (struct intr_frame *f UNUSED)
           break;
       case SYS_EXIT:
       {
-          //printf("SYS_EXIT\n");
-          //hex_dump(f->esp, f->esp, 80, true);
-          if(!is_user_vaddr (esp)){
-              //b_handled = true;
-              // printf("Invalid virtual address: 0x%x\n", esp);
-//              exit_handler(-1);
+          if(!is_valid_range((void*)esp, (void*)esp + 3)) {
+              b_handled = true;
+              exit_handler(-1);
           }
+          //printf("SYS_EXIT\n");
+//          hex_dump(f->esp, f->esp, 80, true);
+//          if(!is_valid_addr (esp)){
+////              printf("True");
+//              //b_handled = true;
+//               printf("Invalid virtual address: 0x%x\n", esp);
+//              exit_handler(-1);
+//          }
           {
               //this is what makes it not spin
               int* status_ptr = esp;
+                //printf("status_ptr: 0x%x,  status_ptr val: %d",status_ptr, *status_ptr);
               exit_handler(*status_ptr);
           }
           b_handled = true;
@@ -203,10 +120,12 @@ syscall_handler (struct intr_frame *f UNUSED)
 //          printf("In sys write....");
 
           //hex_dump(f->esp, f->esp, 50, true);
+          //check esp here
           int fd = *esp;
           esp += 1;
           if (!is_valid_addr(esp))
               exit_handler(-1);
+          //check ptr
           char *buffer = *esp;
           esp += 1;
           unsigned size = *esp;
